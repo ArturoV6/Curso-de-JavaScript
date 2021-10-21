c49743be4534619202be79b7a130d9e3b2275a7e
ArturoV6 <90590091+ArturoV6@users.noreply.github.com>
2021-10-20 17:40:06 -0700

    arrays advanzed

---
+++ src/Metodos de arrays/code2.js
@@ -0,0 +1,18 @@
+//metodos accesores
+
+let frutas = ["manzana", "pera", "naraja", "fresa"];
+
+//nos separa nuestros strings
+let newFrutas = frutas.join(" - ");
+
+console.log(newFrutas);
+
+//---------------------*--------------------//
+
+let names = ["maria", "josefina", "carlos", "dalto"];
+
+//mete los valores seleccionados a un nuevo array
+let newNames = names.slice(0, 2);
+
+console.log(names)
+console.log(newNames);
