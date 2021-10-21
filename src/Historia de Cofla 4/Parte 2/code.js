97af934cc2893995fb2b98484d23ec65ba9aa7bb
ArturoV6 <90590091+ArturoV6@users.noreply.github.com>
2021-10-21 16:50:14 -0700

    historia de colfa 4

---
+++ src/Historia de Cofla 4/Parte 2/code.js
@@ -0,0 +1,42 @@
+const obtenerInfo = (materia) =>{
+    materias = {
+        español: [ "Dalto", " pedro",  " fulano", " maria", " roberto", " fulana"],
+        matematicas: ["Dalto", " pedro", " juana", " fulano", " maria", ],
+        fisica: [ "Dalto", " juana", " fulana", " maria", " roberto", " fulana"],
+        biologia: [ "Dalto", " pedro", " juana",  " roberto", " fulana"]
+    }
+    if (materias[materia] !== undefined) {
+        return [materias[materia],materia];
+    }else if (materias[materia] == undefined) {
+        return false
+    }
+}
+
+const obtenerLoDemas = (materia) =>{
+
+    let info = obtenerInfo(materia);
+
+    if (info !== false) {
+
+        const saludar = newFunction();
+    
+        saludar(info);
+    
+        function newFunction() {
+            return (estudiante) => {
+                let remover = info.splice(1, 4);
+                let remover2 = estudiante[0].slice(1,6);
+                let otraVez = estudiante[0].slice(0,1);
+                     console.log(`El profesor de la clase de ${remover} es ${otraVez} y los alumnos son:${remover2}`);
+            };
+        };
+    
+    }else if (info == false) {
+        console.log("tu materia no esta en el calendario")
+    }else{
+        console.log("?????");
+    };
+};
+
+obtenerLoDemas("biologia");
+obtenerLoDemas("español");
---
+++ src/Objeto Math/code2.js
@@ -0,0 +1,50 @@
+//Propiedades
+
+//Ratio de la circunferencia de un circulo respecto a su diametro
+let numero = Math.PI;
+
+console.log(numero);
+
+//Raiz cuadrada de 1/2
+
+let numero1 = Math.SQRT1_2;
+
+console.log(numero1);
+
+//Raiz cuadrada de 2
+
+let numero2 = Math.SQRT2;
+
+console.log(numero2)
+
+//----------------/Constantes\-------------//
+
+//Constante de Euler
+
+let numero3 = Math.E;
+
+console.log(numero3);
+
+//Logaritmo natural de 2 (ni idea de que es eso)
+
+let numero4 = Math.LN2;
+
+console.log(numero4);
+
+//Lo mismo que arriba pero 10
+
+let numero5 = Math.LN10;
+
+console.log(numero5);
+
+//Logaritmo de E con base 2 (ahora lo entiendo menos)
+
+let numero6 = Math.LOG2E;
+
+console.log(numero6);
+
+//lo mismo que arriba pero con base de 10
+
+let numero7 = Math.LOG10E;
+
+console.log(numero7);
