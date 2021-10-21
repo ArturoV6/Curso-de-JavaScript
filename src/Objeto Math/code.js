ba1092bf7a458931f311a321fcc5a204191c735a
ArturoV6 <90590091+ArturoV6@users.noreply.github.com>
2021-10-21 14:38:02 -0700

    Math2nd commit

--- src/Objeto Math/code.js
+++ src/Objeto Math/code.js
@@ -1,3 +1,5 @@
+//Metodos
+
 //Muestra la raiz cuadrada
 let numero = Math.sqrt(23);
 
@@ -14,3 +16,39 @@ console.log(numero2);
 let numero3 = Math.max(12, 523, 52, 765, 753, 846);
 
 console.log(numero3);
+
+//Devuelve el numero mas pequeño
+
+let numero4 = Math.min(2134, 5, 4, 2, 1, 4, 5,245);
+
+console.log(numero4);
+
+//Devuelve un numero random
+
+let numero5 = Math.random()*99;
+
+console.log(numero5);
+
+//Redondea el numero al valor mas cercano
+
+numeros = Math.round(numero5);
+
+console.log(numeros);
+
+//Redondea el numero a uno menor 
+
+let numeros1 = Math.floor(numero5 + 1);
+
+console.log(numeros1); 
+
+//Este no lo entendi pero parece que no utiliza mucho
+
+let numero6 = Math.fround(9,9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999);
+
+console.log(numero6);
+
+//saca el decimal a secas
+
+let numero7 = Math.trunc(9.4);
+
+console.log(numero7);
