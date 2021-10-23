b3c4bf3d76202dc8c3b0ada669f4128038e0376c
Arturo <arturomadrigal180@gmail.com>
2021-10-23 13:41:11 -0700

    cofla 3 parte 2

--- src/Historia de Cofla 4/Parte 2/code.js
+++ src/Historia de Cofla 4/Parte 2/code.js
@@ -1,55 +1,49 @@
-function obtenerInfo(materia) {
-    materias = {
-        español: ["Dalto", " pedro", " fulano", " maria", " roberto", " fulana"],
-        matematicas: ["Dalto", " pedro", " juana", " fulano", " maria",],
-        fisica: ["Dalto", " juana", " fulana", " maria", " roberto", " fulana"],
-        biologia: ["Dalto", " pedro", " juana", " roberto", " fulana"]
-    };
-    if (materias[materia] !== undefined) {
-        return [materias[materia], materia];
-    } else if (materias[materia] == undefined) {
-        return false;
+    const materias = {
+        fisica: ["Perez","pedro","pepito","cofla","maria"],
+        programacion: ["Dalto","pedro","juan","pepito"],
+        logica: ["Hernandez","pedro","juan","pepito","cofla","maria"],
+        quimica: ["Rodriguez","pedro","juan","pepito","cofla","maria"]
     }
-}
 
-const obtenerLoDemas = (materia) => {
+    const obtenerInformacion = (materia)=>{
 
-    let info = obtenerInfo(materia);
+    if (materias[materia] !== undefined){
+        return [materias[materia],materia,materias];
+        } else {
+        return materias;
+        }
+    }
 
-    if (info !== false) {
+    const mostrarInformacion = (materia)=>{
+    let informacion = obtenerInformacion(materia);
 
-        const saludar = newFunction();
+    if (informacion !== false) {
+    let profesor = informacion[0][0];
+    alumnos = informacion[0];
+    alumnos.shift();
+    console.log((`El profesor de ${informacion[1]} es ${profesor} Los alumnos son: <b style="color:blue">${alumnos}`));
+    }
+    }
 
-        saludar(info);
-
-        
-
-        function newFunction() {
-            return (estudiante) => {
-                let remover = info.splice(1, 4);
-                let remover2 = estudiante[0].slice(1, 6);
-                let otraVez = estudiante[0].slice(0, 1);
-                console.log(`El profesor de la clase de ${remover} es ${otraVez} y los alumnos son:${remover2}`);
-                function obteenrinfo(alumno){
-                    let numeros = 0;
-                    for (infos in info[0]) {
-                        if (info[0][infos].includes(alumno)) {
-                            console.log(`el alumno ${alumno} esta en la clase de ${remover}`)
-                        }
-                    }
-                }
-                 obteenrinfo("pedro")  
-            };
-        };
-    } else if (info == false) {
-        console.log("tu materia no esta en el calendario");
-    } else {
-        console.log("?????");
-    };      
-}
+    const cantidadDeClases = (alumno)=>{
+    let informacion = obtenerInformacion();
+    let clasesPresentes = [];
+    let cantidadTotal = 0;
+    for (info in informacion) {
+        if (informacion[info].includes(alumno)) {
+            cantidadTotal++;
+            clasesPresentes.push(" "+ info);
+        }
+    }
+    return `
+    ${alumno} ${cantidadTotal} clases: ${clasesPresentes}`;
+    }
 
 
+    mostrarInformacion("fisica");
+    mostrarInformacion("quimica");
+    mostrarInformacion("programacion");
+    mostrarInformacion("logica");
 
-obtenerLoDemas("biologia");
-obtenerLoDemas("español");
-obtenerLoDemas("fisica");
+    console.log((cantidadDeClases("cofla")));
+    console.log((cantidadDeClases("pedro")));
