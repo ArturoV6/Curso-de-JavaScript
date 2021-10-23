a335e3cecf9a4d2d38316dbcb615b7ec905d59b1
ArturoV6 <90590091+ArturoV6@users.noreply.github.com>
2021-10-23 00:03:38 -0700

    ls

--- src/Historia de Cofla 4/Parte 2/code.js
+++ src/Historia de Cofla 4/Parte 2/code.js
@@ -12,7 +12,7 @@ function obtenerInfo(materia) {
     }
 }
 
-function obtenerLoDemas(materia) {
+const obtenerLoDemas = (materia) => {
 
     let info = obtenerInfo(materia);
 
@@ -22,21 +22,34 @@ function obtenerLoDemas(materia) {
 
         saludar(info);
 
+        
+
         function newFunction() {
             return (estudiante) => {
                 let remover = info.splice(1, 4);
                 let remover2 = estudiante[0].slice(1, 6);
                 let otraVez = estudiante[0].slice(0, 1);
                 console.log(`El profesor de la clase de ${remover} es ${otraVez} y los alumnos son:${remover2}`);
+                function obteenrinfo(alumno){
+                    let numeros = 0;
+                    for (infos in info[0]) {
+                        if (info[0][infos].includes(alumno)) {
+                            console.log(`el alumno ${alumno} esta en la clase de ${remover}`)
+                        }
+                    }
+                }
+                 obteenrinfo("pedro")  
             };
         };
-
     } else if (info == false) {
         console.log("tu materia no esta en el calendario");
     } else {
         console.log("?????");
-    };
+    };      
 }
+
+
 
 obtenerLoDemas("biologia");
 obtenerLoDemas("español");
+obtenerLoDemas("fisica");
