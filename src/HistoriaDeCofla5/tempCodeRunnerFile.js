66468cdfa57f1d45ce6ffa60bfcaffa7cc72666d
Arturo <arturomadrigal180@gmail.com>
2021-10-24 03:13:05 -0700

    historia cofla 5

---
+++ src/HistoriaDeCofla5/tempCodeRunnerFile.js
@@ -0,0 +1,27 @@
+const materias = {
+    fisica: [4, 40, 25],
+    biologia: [7, 34, 37],
+    español: [2, 14, 37]
+};
+
+function calcular(asistencia, examen, trabajitos) {
+    const result = asistencia + examen + trabajitos;
+    return result;
+}
+
+const aprobar = () =>{
+    for (materia in materias) {
+        const cantidadMinima = 60;
+        let asistencia = materias[materia][0];
+        let examen = materias[materia][1];
+        let trabajitos = materias[materia][2];
+        let valorFinal = calcular(asistencia, examen, trabajitos);
+        if (valorFinal >= 60) {
+            console.log(`feliciddes aprobaste ${materia} con un ${valorFinal}`);
+        } else if(valorFinal < cantidadMinima){
+            console.log(`la cagaste reprobaste ${materia} con un ${valorFinal}`);
+        }
+    }
+}
+
+aprobar();
